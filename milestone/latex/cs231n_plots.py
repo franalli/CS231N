1f99460788d47fb125e7ec54aaf091503ca203e9
Josh King <jrkingjr9@gmail.com>
2017-06-11 01:00:29 +0000

    blah

--- milestone/latex/cs231n_plots.py
+++ milestone/latex/cs231n_plots.py
@@ -2,30 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.axes as axes
 
-baseline=[0.2858082191780822,0.4055342465753425,0.47715068493150686,0.5312602739726028,0.5726027397260274]
-resnet18=[.3819178082191781,0.514317491748194,0.5919481845571832,0.646837571838181,0.6873190129101192]
-resnet18_proj=[0.36747945205479454,0.5028767123287671,0.5813150684931507,0.6330958904109589,0.6727671232876712]
-resnet34=[0.38652054794520546,0.5182739726027398,0.5958082191780822,0.6491232876712328,0.6886301369863014]
-resnet50=[0.37583561643835617,0.5126849315068494,.591013698630137,0.6428219178082192,0.682958904109589]
-
-x=[1,2,3,4,5]
-
-plt.figure(figsize=(20,20))
-baseline,=plt.plot(x,baseline,linewidth=3)
-resnet18,=plt.plot(x,resnet18,linewidth=3)
-resnet18_proj,=plt.plot(x,resnet18_proj,linewidth=3)
-resnet34,=plt.plot(x,resnet34,linewidth=3)
-resnet50,=plt.plot(x,resnet50,linewidth=3)
-plt.axis([1,5,0,1.0])
-
-plt.xlabel('Top-n',size=25)
-plt.ylabel('Accuracy',size=25)
-
-plt.xticks(np.linspace(1, 5, num=5, endpoint=True, retstep=False, dtype=None),fontsize = 20)
-plt.yticks(np.linspace(0, 1.0, num=21, endpoint=True, retstep=False, dtype=None),fontsize = 20)
-
-
-plt.legend([baseline,resnet18,resnet18_proj,resnet34,resnet50],['Baseline','ResNet 18','ResNet 18 Proj','ResNet 34','ResNet 50'],prop={'size':25})
-plt.title("Accuracy Comparison for top-n accuracy when varying n",size=25)
-#plt.show()
-plt.savefig('top-n.png')
+y=[14108, 4809, 2830, 1946, 1442, 1080, 873, 741, 631, 569, 459, 422, 377, 347, 310, 259, 259, 268, 209, 190, 185, 184, 144, 177, 113, 121, 138, 113, 116, 121, 121, 86, 82, 69, 89, 81, 69, 73, 75, 55, 65, 38, 61, 73, 53, 46, 50, 36, 50, 46, 38, 48, 34, 28, 35, 40, 26, 43, 41, 43, 45, 36, 35, 29, 23, 19, 15, 25, 25, 18, 18, 33, 19, 11, 19, 20, 21, 17, 21, 13, 18, 13, 26, 19, 16, 21, 20, 23, 16, 10, 15, 11, 10, 14, 22, 13, 6, 7, 10, 10, 18, 10, 3, 5, 7, 8, 7, 6, 13, 6, 10, 8, 10, 6, 9, 6, 6, 4, 5, 12, 4, 6, 14, 7, 5, 7, 4, 8, 3, 4, 2, 4, 5, 4, 5, 4, 4, 7, 5, 2, 2, 3, 3, 2, 4, 4, 4, 7, 2, 7, 6, 5, 3, 4, 3, 2, 2, 1, 5, 6, 5, 3, 5, 3, 3, 1, 1, 1, 1, 2, 2, 3, 4, 0, 3, 2, 1, 1, 4, 2, 2, 4, 2, 0, 1, 3, 3, 3, 5, 3, 2, 2, 5, 2, 0, 0, 0, 0, 1, 2, 2, 2, 1, 1, 2, 2, 1, 1, 0, 1, 0, 0, 1, 1, 2, 1, 0, 1, 0, 1, 1, 0, 1, 2, 0, 2, 0, 1, 1, 1, 0, 2, 0, 0, 0, 1, 1, 1, 0, 3, 1, 1, 0, 0, 0, 0, 1, 0, 3, 2, 0, 0, 1, 1, 0, 3, 1, 2, 1, 0, 1, 1, 0, 1, 1, 1, 2, 2, 3, 0, 0, 1, 0, 0, 0, 2, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y=[v/sum(y) for v in y]
+plt.plot(range(len(y)),y)
+plt.title("Distribution of Rank of Correct Label Within Prediction")
+plt.xlabel("Rank")
+plt.ylabel("Frequency")
+plt.show()
